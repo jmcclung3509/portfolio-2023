8aa6609c7dc09ebd33dffd385eec2f4c396dcc8d
Jessica Turner <jmcclung3509@gmail.com>
2023-10-03 07:19:27 -0700

    Uploaded pages, tailwind.config, public

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<main class="space-y-36 lg:space-y-52 relative">
+		<section class="pt-36 xl-pt-40 fold animate hero container mx-auto">
+			<h1 class="text-4xl text-purple-dark"> Lets try this out!</h1>
+		</section>
+	</main>
+</template>
+<script setup>
+import '@/assets/css/tailwind.css'
+
+const route = useRoute()
+
+const data = reactive({
+	mobileMenuOpen: false
+})
+</script>
